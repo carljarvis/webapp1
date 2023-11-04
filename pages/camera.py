bbbccece10c28a663f61351d6d87fb1e784ff54d
Carl Jarvis <carl@carljarvis.org>
2023-11-04 12:43:53 -0500

    Add Camera Functionality

--- pages/camera.py
+++ pages/camera.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from PIL import Image
 
-st.camera_input('camera')
+camera_image = st.camera_input('camera')
+print(camera_image)
+
+if camera_image != None:
+
+    img =  Image.open(camera_image)
+    gray_img = img.convert('L')
+    st.image(gray_img)
